45239cf841e973820ca32b0a0b2fc1b4f7512fa8
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-07-18 20:40:22 +0200

    detail pokemon

--- src/components/PokemonDetail/PokemonDetail.js
+++ src/components/PokemonDetail/PokemonDetail.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import ProgressBar from "@ramonak/react-progress-bar";
+import PokemonEvolutions from "../Evolutions/PokemonEvolutions";
+import Evolution from "../Evolutions/Evolutions";
 
 const PokemonDetail = () => {
   const { id } = useParams();
   const [pokemon, setPokemon] = useState([]);
+  const [evolutionChain, setEvolutionChain] = useState([]);
+
   useEffect(() => {
     async function getPokemon() {
       let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
       response = await response.json();
+
       setPokemon(response);
     }
 
@@ -19,7 +24,7 @@ const PokemonDetail = () => {
   const listStat =
     pokemon.stats &&
     pokemon.stats.map((stat, index) => (
-      <div className="stat">
+      <div className="stat" key={stat.id}>
         <p key={index}>{stat.stat.name} </p>
 
         <ProgressBar
@@ -30,14 +35,31 @@ const PokemonDetail = () => {
         />
       </div>
     ));
+
   return (
-    <div className="detail">
-      <h1>{pokemon.name}</h1>
-      <p>height: {pokemon.height} cm</p>
-      <div className="stats">
-        <h1>stats</h1>
-        <div className="stat-container">{listStat}</div>
+    <div className="detail px-10 pt-6 ">
+      <div className="title flex bg-green-400">
+        <h1>{pokemon.name}</h1>
+        <p className="ml-3">#{pokemon.id}</p>
       </div>
+      <div className="content flex justify-between">
+        <p>height: {pokemon.height} cm</p>
+        <div className="abilities">
+          <h2>Abilities:</h2>
+          <ul>
+            {pokemon.abilities &&
+              pokemon.abilities.map((ability, index) => (
+                <li key={index}>{ability.ability.name}</li>
+              ))}
+          </ul>
+        </div>
+
+        <div className="stats w-2/6">
+          <h1>stats</h1>
+          <div className="stat-container">{listStat}</div>
+        </div>
+      </div>
+      <PokemonEvolutions />
     </div>
   );
 };
