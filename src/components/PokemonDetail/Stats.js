fae923514ff13d9a1782f28c024ce610db1ad27d
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-07-21 00:32:45 +0200

    stats pokemon

--- src/components/PokemonDetail/Stats.js
+++ src/components/PokemonDetail/Stats.js
@@ -1,8 +1,25 @@
 import React from "react";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 function Stats(props) {
-  console.log(props);
-  return <div>stats</div>;
+  //console.log(props);
+  const listStat =
+    props.stats &&
+    props.stats.map((stat, index) => (
+      <div className="stat" key={index}>
+        <p key={index} className="font-bold">
+          {stat.stat.name}
+        </p>
+
+        <ProgressBar
+          key={stat.id}
+          completed={stat.base_stat}
+          bgColor={stat.base_stat > 50 ? "green" : "red"}
+          animateOnRender={true}
+        />
+      </div>
+    ));
+  return <div className="py-2 px-10">{listStat}</div>;
 }
 
 export default Stats;
